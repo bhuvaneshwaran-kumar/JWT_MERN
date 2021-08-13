0aa5e5b3694e3db4dcc2f7d3c8fd3d718dd5f138
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2021-08-13 11:22:34 +0530

    settedUp Signup Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import { Route } from 'react-router-dom'
+
+import Nav from './components/Nav'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+
 function App() {
   return (
     <div className="App">
-      Hellow World....
+      <Nav />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/signup" component={SignupPage} />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 import './index.css';
+import AuthPovider from './context/AuthContext'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AuthPovider >
+        <App />
+      </AuthPovider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
