64d291d34cdccad1451c1d784111db620bd1b153
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2021-08-14 08:24:18 +0530

    setted up server.js server running in port : 8081

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+
+
+const { CORS_ORIGIN, MONGO_DB_URL, PORT } = require('./constants')
+console.log(PORT)
+const main = async () => {
+    await mongoose.connect(MONGO_DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+    })
+
+    const app = express()
+
+    let CORS_ORIGINS = CORS_ORIGIN.split(' ')
+    app.use(
+        cors({
+            origin: CORS_ORIGINS,
+            credentials: true
+        })
+    )
+
+    app.use(cookieParser())
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+
+    app.get("/", (_, res) => res.send('Hello World...!'))
+
+    app.listen(PORT, () => console.log(`URL: http://localhost:${PORT}`))
+}
+
+main()
+
+
+/* ---------> Without using cors module
+
+// app.all('*', (req, res, next) => {
+//     console.log(req.header('origin'))
+//     var origin = cors.origin.indexOf(req.header('origin').toLowerCase()) > -1 ? req.headers.origin : cors.default;
+//     res.header("Access-Control-Allow-Origin", origin);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//     next()
+
+
+// })*/
